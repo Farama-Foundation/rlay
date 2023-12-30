edd419af66c20e8216206d7a2291c3a7c514991c
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2023-12-30 20:24:06 +0700

    Remove incorrect test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,2 @@
 """Tests of the core functionalities."""
 import pytest
-from rlay import square
-
-@pytest.mark.parametrize("value", list(range(100)))
-def test_square(value: int):
-    assert square(value) == value**2
