26e350a7bea4e3ccb9ae3c9cea884a2ed2fdb31c
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2023-12-30 23:29:43 +0700

    Minor updates

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
-#python -m grpc_tools.protoc -I./rlay/protos/ --python_out=./rlay/ --pyi_out=./rlay/ --csharp_out=./rlay/ ./rlay/protos/gym_grpc/gym.proto
-#protoc -I./rlay/protos/ --python_out=./rlay/ --pyi_out=./rlay/ --csharp_out=./rlayEnv/Assets/ ./rlay/protos/gym_grpc/gym.proto
-
+#!bash
 protoc -I./protos/ --python_out=./rlay/ --pyi_out=./rlay/ --rust_out ./rlay-rs/src/ ./protos/gym_grpc/gym_rlay.proto
-
-#python -m grpc_tools.protoc -I./rlay/protos/ --python_out=./rlay/ --pyi_out=./rlay/ --grpc_python_out=./rlay/ ./rlay/protos/gym_grpc/gym.proto
-#protoc -I./rlay/protos/ --python_out=./rlay/ ./rlay/protos/gym_grpc/gym.proto
